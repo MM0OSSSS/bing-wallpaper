367e0ef59790a6dea32b202ba7534e12f1c73a62
MM0OSSSS <127008790+MM0OSSSS@users.noreply.github.com>
2023-05-13 16:20:00 +0800

    Update pabing.py

--- pabing.py
+++ pabing.py
@@ -1,24 +1,20 @@
 def pa():
     import requests
-    import bs4
-    head = {
-        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
-        } 
-    url = "https://cn.bing.com/"
-
-    res = requests.get(url, headers=head)
-    res.encoding = res.apparent_encoding
-
-    soup = bs4.BeautifulSoup(res.text, "lxml")
-
-    tags = soup.find_all("div", class_="img_cont")
-
-    a = tags[0]
-    b = str(a)
-    c = b[51:276]
-    c = c[:-151]
-    print(c)
-    r = requests.get(c)
-    pic = r.content
+    url = "https://cn.bing.com/HPImageArchive.aspx?n=1&format=js&idx=0"
+    res = requests.get(url)
+    res.encoding = "UTF-8"
+    a = res.text
+    b = a.split(",")
+    web = b[4]
+    web = web.split(":")
+    web = web[1]
+    web = web[1:-1]
+    print(web)
+    f = "https://s.cn.bing.net"
+    b = "_1920x1080.jpg"
+    newweb = f + web + b
+    print(newweb)
+    res = requests.get(newweb)
+    pic = res.content
     with open("img.jpg","wb") as file:
        file.write(pic)
